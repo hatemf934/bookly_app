518ed9e28443438140cafc385e9d7b04a84ceb67
hatemfathy <hatemf934@gmail.com>
2025-05-11 01:32:25 +0300

    create DetailsBookView

---
+++ lib/Features/home/presentation/views/details_book_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/details_book_view_body.dart';
+import 'package:flutter/material.dart';
+
+class DetailsBookView extends StatelessWidget {
+  const DetailsBookView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DetailsBookViewBody();
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/details_book_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailsBookViewBody extends StatelessWidget {
+  const DetailsBookViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
